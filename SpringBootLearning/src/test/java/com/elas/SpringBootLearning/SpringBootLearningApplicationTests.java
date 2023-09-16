2cc5bc53f321faed335795e4f475b4e21385d720
elamp <elamp@DESKTOP-4K9VT94>
2023-09-16 11:17:09 +0530

    Initial commit

---
+++ SpringBootLearning/src/test/java/com/elas/SpringBootLearning/SpringBootLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elas.SpringBootLearning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootLearningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
